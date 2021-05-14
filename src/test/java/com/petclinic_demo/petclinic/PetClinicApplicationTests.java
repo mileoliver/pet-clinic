6bf445f8da4443d872fcb78efefc3a47e9a6ec45
mileoliver <mile.oliver95@gmail.com>
2021-05-14 13:14:49 +0200

    Initializing

---
+++ src/test/java/com/petclinic_demo/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petclinic_demo.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
