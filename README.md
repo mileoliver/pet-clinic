fd0ca1e09a6957269851d07ad8b7df1cd7b83be7
mileoliver <mile.oliver95@gmail.com>
2021-10-09 11:30:18 +0200

    created Specialty, updated README

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # pet-clinic
 
-#during helloAI course, temporarily on hold
-
 pet-clinic demo according to sfg udemy course
---
+++ pet-clinic-data/src/main/java/com/petclinic_demo/petclinic/model/Specialty.java
@@ -0,0 +1,13 @@
+package com.petclinic_demo.petclinic.model;
+
+public class Specialty extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
